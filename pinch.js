b4e8f5253b62b7074a4dfe040ad49dea95d3558c
Akash kumar Singh <lengendmaster7462@gmail.com>
2026-04-21 15:32:32 +0530

    Add files via upload

---
+++ pinch.js
@@ -0,0 +1,6 @@
+function toggleNavbar() {
+    var navLinks = document.querySelector('.nav-links');
+    var burger = document.querySelector('.burger');
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('active');
+}
